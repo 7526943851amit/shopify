02635de33ccc9e5022722269672632608e558f4e
Amit sharma <141321471+7526943851amit@users.noreply.github.com>
2023-12-08 15:14:09 +0530

    Update shopify condiiton.php

--- shopify condiiton.php
+++ shopify condiiton.php
@@ -1,1 +1,35 @@
-  {% if template == "page.shop" or page.handle contains 'affiliate-program' or template == 'collection.deals' or template == "product" or template == "customers/login" or template == "list-collections" or template == "collection" or template == "page.contact-us" or template == "page.bonus-catalog" or template == "cart" or template == "search" %}
+  {% if template == "page.shop" or page.handle contains 'affiliate-program'
+or template == 'collection.deals' or template == "product" or template == "customers/login" or template == "list-collections" or template == "collection" or template == "page.contact-us" or template == "page.bonus-catalog" or template == "cart" or template == "search" %}
+{% if product.title == 'amit' %}
+  Hey Kevin!
+{% elsif product.title == 'anonymous' %}
+  Hey Anonymous!
+{% else %}
+  Hi Stranger!
+{% endif %}
+<h1>{{ section.settings.heading }}</h1><br>
+<h1>{{ section.settings.des_content }}</h1>
+{% schema %}
+{
+"name": "amit",
+"class": "amit",
+"settings": [
+{
+"type"  : "text",
+"id"    : "heading",
+"label" : " Main Heading"
+},
+{
+"type"  : "text",
+"id"    : "des_content",
+"label" : "Description "
+}
+],
+"presets": [
+{
+"name"      : "Diet Plan",
+"category"  : "Custom"
+}
+]
+}
+{% endschema %}
