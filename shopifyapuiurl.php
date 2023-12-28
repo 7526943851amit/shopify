53de5e23c0d95524b66cedc9258c64082661fb07
Amit sharma <141321471+7526943851amit@users.noreply.github.com>
2023-12-28 17:20:52 +0530

    Update shopifyapuiurl.php

--- shopifyapuiurl.php
+++ shopifyapuiurl.php
@@ -1,7 +1,7 @@
 issmien pehele api key hai fir token hai
 <!-- https://c3060c971def7799e29cb7e43c6ec174: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@5f9e44.myshopify.com/admin/api/2023-10/products.json -->
 
+c3060c971def7799e29cb7e43c6ec174 shopify api key ha je fir pswrd hai gfir url se copy kiya hua je hai 
 
-
-
+https://anytimetoolandauto.com/admin/api/2023-10/products.json
 <!-- https://anytimetoolandauto.com/admin/api/2023-10/products.json -->
