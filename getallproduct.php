a1823b305e032747bffc85bc1ea6fb3082fa0f8b
Amit sharma <141321471+7526943851amit@users.noreply.github.com>
2024-01-08 16:01:10 +0530

    Update getallproduct.php

--- getallproduct.php
+++ getallproduct.php
@@ -5,6 +5,12 @@
     <h5>{{ product.featured_image | image_url: width:400 | img_tag }}</h5>
 {% endfor %}
 
+{% for product in collections.all.products  %}
+    <h1>{{ product.title }}</h1>
+    <h5>{{ product.description }}</h5>
+    <h5>{{ product.featured_image | image_url: width:400 | img_tag }}</h5>
+{% endfor %}
+
 {% section 'mysection' %}
 {% assign customproducts = all_products['hh'] %}
 {{ customproducts.title }}
